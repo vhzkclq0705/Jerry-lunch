b46b3378317a4b30714d575b9138ded86c5346da
vhzkclq0705 <vhzkclq0705@naver.com>
2025-02-07 20:29:25 +0900

    메인 페이지 구현

---
+++ Main.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+# --UI--
+
+st.title('점심 데이터 EDA 👋')
+
+st.write('''
+![img](https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnx0j7LHrzyLMgnVrf1BphLuRPH1b-dZO20w&s)
+
+🍱🥗🍜🍕🍣🥩
+
+- 입력한 점심 데이터를 분석 및 시각화 합니다.
+- 점심 데이터는 LG U+ Why Not SW Camp 4기 멤버들이 입력하였습니다.
+
+- **사이드 바에서 원하는 분석 항목을 선택하세요!**
+
+
+## 분석 내용
+
+- 점심 메뉴 인기 순위 📊
+- 요일별 선호 음식 🍽️
+- 점심 비용 분포 💰
+- 특정 메뉴에 대한 트렌드 분석 📈
+- 기록하지 않은 사람 색출 🕵️
+''')
