5190306e5a06f8308e44bdc12c6f2102ce0808bc
vhzkclq0705 <vhzkclq0705@naver.com>
2025-02-09 19:44:06 +0900

    Rank 페이지 구현 완료

--- Managers/rank.py
+++ Managers/rank.py
@@ -5,10 +5,15 @@ class Rank_manager:
     def __init__(self, db):
         self.db = db
 
-    def get_rank_of_lunch(self) -> pd.DataFrame:
-        data = db.select_data()
-        df = pd.DataFrame(data, columns=['menu_name'])
-        grouped_df = df.groupby('menu_name').size().reset_index(name='count')
-        sorted_df = grouped_df.sort_values(by='count', ascending=False)
+    def get_table(self) -> pd.DataFrame:
+        lunch_menus = self.db.get_lunch_menus()
+        lunch_menus_dict = [vars(lunch_menu) for lunch_menu in lunch_menus]
 
-        return sorted_df
+        df = (
+            pd.DataFrame(lunch_menus_dict)
+            .groupby('menu_name').size().reset_index(name='count')
+            .sort_values(by='count', ascending=False)
+            .reset_index(drop=True)
+        )
+
+        return df
