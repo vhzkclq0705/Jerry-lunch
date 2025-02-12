bef1afae867b295fe1f602b466ae816e75203a8b
vhzkclq0705 <vhzkclq0705@naver.com>
2025-02-12 22:17:46 +0900

    데이터베이스 웹 연동

--- src/jerry_lunch/config/db_config.py
+++ src/jerry_lunch/config/db_config.py
@@ -2,13 +2,10 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
-db_name = os.getenv('DB_NAME')
-
 db_config = {
-    "dbname": db_name,
-    "user": os.getenv('DB_USERNAME'),
-    "password": os.getenv('DB_PASSWORD'),
-    "host": os.getenv('DB_HOST'),
-    "port": os.getenv('DB_PORT')
+    "dbname": os.getenv("POSTGRES_DATABASE"),
+    "user": os.getenv("POSTGRES_USER"),
+    "password": os.getenv("POSTGRES_PASSWORD"),
+    "host": os.getenv("POSTGRES_HOST"),
+    "port": os.getenv("POSTGRES_PORT", "5432")
 }
